32c139ed67b6704e3f5a45c61b98ade713aea783
Catherine Siller <cathysiller503@gmail.com>
2015-09-01 21:18:39 -0500

    Personal Website for Connote Chuleewah
    Personal Website for Connote Chuleewah— front page will have sample
    work, also includes about/resume page

---
+++ js.js
@@ -0,0 +1,16 @@
+$('a').click(function(){return false;});
+
+var nav, scrollTop,
+    win = $(window);
+
+nav = {
+  nav: $('#nav'),
+  setTop: function(top) {
+    this.nav.css('-webkit-transform', 'matrix(1, 0, 0, 1, 0, -' + Math.round(top/2) + ')');
+  }
+};
+
+win.scroll(function(){
+  scrollTop = win.scrollTop();
+  nav.setTop(scrollTop);
+});
